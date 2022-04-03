7e3a7cb6b698e2413e741862f125e2b49a07b94c
m-shehzad <shehzadboii@gmail.com>
2022-04-03 15:34:42 +0530

    dynamic underline added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     window.scrollTo(0, 0);
 });
 
+//navbar function
 let prevScrollPos = window.scrollY;
 window.addEventListener('scroll',()=>{
     //navbar background changer
@@ -17,5 +18,20 @@ window.addEventListener('scroll',()=>{
         document.querySelector('header').style.transform = 'translateY(-100%)';
     }
     prevScrollPos = currentScrollPos;
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
 })
+
+
+//underline function
+let section = document.querySelectorAll('section');
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop-150;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top<offset+height){
+            document.querySelector("#"+id+' .underline').style.transform="scaleX(1)";
+        }
+    })
+}

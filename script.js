f054cdda32492a895fe67f5142e2445b88f5f2d8
m-shehzad <shehzadboii@gmail.com>
2022-04-11 23:37:21 +0530

    Electrical page v1.0 done

--- script.js
+++ script.js
@@ -50,7 +50,25 @@ gsap.from('.stg3',{
 //     stagger:.2,
 //     duration:.5
 // })
-
+// -------------Animations for department---------------
+gsap.to('.stg-E1',{
+    opacity:100,
+    y:-50,
+    delay:0.5,
+    stagger:.5,
+    duration:.5
+})
+gsap.from('.stg-E2',{
+    scrollTrigger:{
+        trigger:'#responsibility',
+        // markers:true,
+        start:'top center'
+    },
+    opacity:0,
+    y:50,
+    stagger:.3,
+    duration:.5
+})
 
 const navLinks = document.querySelector('header nav');
 const header = document.querySelector('header');

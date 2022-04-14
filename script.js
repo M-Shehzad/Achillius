e9b1f3ca8b233d9868236944a950f64bd181bf5d
m-shehzad <shehzadboii@gmail.com>
2022-04-14 13:54:51 +0530

    respective departments added

--- script.js
+++ script.js
@@ -50,25 +50,7 @@ gsap.from('.stg3',{
 //     stagger:.2,
 //     duration:.5
 // })
-// -------------Animations for department---------------
-gsap.to('.stg-E1',{
-    opacity:100,
-    y:-50,
-    delay:0.5,
-    stagger:.5,
-    duration:.5
-})
-gsap.from('.stg-E2',{
-    scrollTrigger:{
-        trigger:'#responsibility',
-        // markers:true,
-        start:'top center'
-    },
-    opacity:0,
-    y:50,
-    stagger:.3,
-    duration:.5
-})
+
 
 const navLinks = document.querySelector('header nav');
 const header = document.querySelector('header');
